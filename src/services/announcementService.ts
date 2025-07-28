6435ea5f1f696f063150db48662554a700c51aeb
Akash Thakur <iamakash0218@gmail.com>
2025-07-28 15:43:56 +0530

    Add test cases for services

--- src/services/announcementService.ts
+++ src/services/announcementService.ts
@@ -3,6 +3,8 @@ import axiosInstance from "./axiosInstance";
 export const getAnnouncements = async (role:string) => {
     try {
         const res = await axiosInstance.get(`/announcements/visible-to-me?role=${role}`);
+        console.log(res);
+        
         return res.data;
     } catch (error) {
         console.error("Error fetching announcements:", error);
